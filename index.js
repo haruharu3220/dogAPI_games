36fb0ef3d2f8169872edd78e59051fb67437ef03
Hayato <hayato.314.nico@gmail.com>
2023-06-02 20:22:23 +0900

    DogAPI

---
+++ index.js
@@ -0,0 +1,27 @@
+//let url = `https://dog.ceo/api/breed/${breed}/images`;
+const options = {
+  method: "get",
+};
+
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      test: "これはテストです",
+      dogImage: "",
+      breeds: ["Affenpinscher", "Akita", "Boxer"],
+      breed: "Akita",
+    };
+  },
+  methods: {
+    async getDogImage() {
+      const response = await fetch(
+        `https://dog.ceo/api/breeds/list/all`,
+
+        options
+      ).then((response) => response.json());
+      console.log(response);
+      //   this.dogImage = response.message;
+    },
+  },
+});
